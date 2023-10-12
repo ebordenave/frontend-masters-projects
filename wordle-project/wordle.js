fbb64ee58401dfa8d9ce2a73422107703d55e30b
Edward Bordenave <edward.bordenave@gmail.com>
2023-10-11 21:18:46 -0400

    refactor: update event listeners

--- wordle-project/wordle.js
+++ wordle-project/wordle.js
@@ -1,18 +1,13 @@
-let textarea = document.getElementById("test-target")
 
+window.addEventListener("keydown", (e) => {
+  console.log(`${e.key.toUpperCase()}`)
+//   logic for targeting innerHTML grid box here
+})
 
-textarea.addEventListener("input", (e) => {
-  console.log(e.data.toUpperCase());
-});
-
-document.addEventListener("DOMContentLoaded", function () {
-  let handleButtonClick = document.getElementById("keyboardButtonSection");
-
-  handleButtonClick.addEventListener("click", (e) =>{
-    if (e.target.tagName === "BUTTON") {
-      console.log(`${e.target.textContent}`)
-    }
-  })
+window.addEventListener("click", (e) => {
+  if (e.target.tagName === "BUTTON") {
+    console.log(`${e.target.textContent}`)
+  }
 })
 
 
