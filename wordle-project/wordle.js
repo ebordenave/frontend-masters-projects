babea885811909eefbe340f2bf73107cb3a21c51
Edward Bordenave <edward.bordenave@gmail.com>
2023-10-19 11:42:19 -0400

    refactor: render eventlistener results

--- wordle-project/wordle.js
+++ wordle-project/wordle.js
@@ -1,24 +1,52 @@
+let currentId = 0
 
 const handleKeyboardKeydown = (e) => {
+  // format to the keydown to always be Uppercase
   const keydownEventValue = e.key.toUpperCase()
 
-  console.log(`${keydownEventValue}`)
+  // if key is 1 letter
+  if (e.key.length === 1) {
 
-  // if Enter was pressed AND guessed word is a valid word (define valid word)
-  // then submit guess
+    // declare a div and a corresponding ID to a variable
+    const div = document.getElementById(`${currentId}`)
+
+    // if div exists and the div class contains wordleBox
+    if (div && div.classList.contains("wordleBox")) {
+
+      // the text content will equal the keydowneventvalue
+
+      div.textContent = keydownEventValue
+      // the text content will equal the keydowneventvalue
+      currentId += 1
+    }
+
+  }
+
   if (keydownEventValue === "ENTER") {
-    console.log("enter was pressed")
+    console.log("submitting Guess")
   }
 }
+
 document.body.addEventListener("keydown",handleKeyboardKeydown)
 
 const onscreenKeyboard = document.getElementById("keyboardButtonSection")
 
 const handleOnscreenKeyboardClick = (e) => {
   if (e.target.tagName === "BUTTON"){
-    console.log(`${e.target.textContent}`)
+    document.getElementById(`${currentId}`).innerHTML= e.target.textContent
+    currentId += 1
   }
 }
 
 onscreenKeyboard.addEventListener("click",handleOnscreenKeyboardClick)
+
+const createWordleBoxIdNumbers = () => {
+  const wordleBoxes = document.querySelectorAll(".wordleBox")
+
+  wordleBoxes.forEach((box,index)=> {
+    box.id = `${index}`
+  })
+}
+createWordleBoxIdNumbers()
+
 
